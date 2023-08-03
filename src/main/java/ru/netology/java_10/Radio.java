283e955024b4743223988d3c6048337c1cc31d62
Alena Katkova <alena.katkova1996@gmail.com>
2023-08-03 19:46:59 +0300

    setting gitignore

---
+++ src/main/java/ru/netology/java_10/Radio.java
@@ -0,0 +1,55 @@
+package ru.netology.java_10;
+public class Radio {
+
+    private int currentStation;
+    private int volume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation >= 0 && currentStation <= 9) {
+            this.currentStation = currentStation;
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            this.volume = volume;
+        }
+    }
+
+    public void nextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+
+}
